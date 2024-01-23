10cd073ed70f36c15a17d2ba87bb013384abe3c0
Thaiboi <quangthai03112001@gmail.com>
2024-01-23 18:01:29 +0700

    clean up navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,11 @@ import { Link, useParams } from 'react-router-dom';
 import logo from '../assets/logo.webp';
 import SearchInput from './SearchInput';
 import ColorModeSwitch from './Selector/ColorModeSwitch';
+import useGameQueryStore from '../statesStore/gameQueryStore';
 
 const NavBar = () => {
   const { selectedGameId } = useParams();
-
+  const setDefaultValue = useGameQueryStore((s) => s.setDefaultValue);
   return (
     <HStack
       padding='5px 10px 0 10px'
@@ -16,7 +17,7 @@ const NavBar = () => {
       border='none'
       zIndex={10}
     >
-      <Link to='/'>
+      <Link to='/' onClick={() => setDefaultValue()}>
         <Image src={logo} boxSize='3.5rem' objectFit='cover' />
       </Link>
       <SearchInput />
